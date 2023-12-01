d582b91bd14491a54c3e86daf3b1db836850e73f
Katerina Skroumpelou <sk.katherine@gmail.com>
2023-12-01 14:34:48 +0200

    two react apps

---
+++ rapp2-e2e/cypress.config.ts
@@ -0,0 +1,14 @@
+import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
+
+import { defineConfig } from 'cypress';
+
+export default defineConfig({
+  e2e: {
+    ...nxE2EPreset(__filename, {
+      cypressDir: 'src',
+      bundler: 'vite',
+      webServerCommands: { default: 'nx run rapp2:serve' },
+    }),
+    baseUrl: 'http://localhost:4200',
+  },
+});
---
+++ rapp2/vite.config.ts
@@ -0,0 +1,44 @@
+/// <reference types='vitest' />
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
+
+export default defineConfig({
+  root: __dirname,
+  cacheDir: '../node_modules/.vite/rapp2',
+
+  server: {
+    port: 4200,
+    host: 'localhost',
+  },
+
+  preview: {
+    port: 4300,
+    host: 'localhost',
+  },
+
+  plugins: [react(), nxViteTsPaths()],
+
+  // Uncomment this if you are using workers.
+  // worker: {
+  //  plugins: [ nxViteTsPaths() ],
+  // },
+
+  build: {
+    outDir: '../dist/rapp2',
+  },
+
+  test: {
+    globals: true,
+    cache: {
+      dir: '../node_modules/.vitest',
+    },
+    environment: 'jsdom',
+    include: ['src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
+
+    coverage: {
+      reportsDirectory: '../coverage/rapp2',
+      provider: 'v8',
+    },
+  },
+});
